bd5c7bd74b8dc8406a1875776aabfd9284e3649b
srikanth <cs@gmail.com>
2023-07-12 15:21:56 +0530

    added new project PySelPytestProject

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -1,2 +1,14 @@
+import openpyxl
 class HomePageData1:
     test_homepage_data=[{"Name":"csr1","email":"cs1@gamil.com","Password":"cs1234","Gender":"Female"},{"Name":"csr2","email":"cs2@gamil.com","Password":"cs1234","Gender":"Female"}]
+    @staticmethod
+    def getTestData(test_case_name): #self not required, beacuse above @staticmethod defined
+        dict={}
+        book=openpyxl.load_workbook("C:\\Users\\srika\\OneDrive\\Desktop\\Atmntestdta\\formtestdata.xlsx")
+        sheet=book.active
+        for i in range(1, sheet.max_row + 1):
+            if sheet.cell(row=i, column=1).value == test_case_name:
+                for j in range(1, sheet.max_column + 1):
+                    dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+        return[dict]
+
--- TestData/excelTestData.py
+++ TestData/excelTestData.py
@@ -1,5 +1,5 @@
 import openpyxl
-book=openpyxl.load_workbook("C:\\Users\\srika\\OneDrive\\Desktop\\exceldata.xlsx")
+book=openpyxl.load_workbook("C:\\Users\\srika\\OneDrive\\Desktop\\Atmntestdta\\exceldata.xlsx")
 sheet=book.active
 cell=sheet.cell(row=1,column=2)
 print(cell.value)
@@ -12,5 +12,6 @@ print(sheet['A2'].value)
 #     print(sheet.cell(row=i,column=i).value)
 
 for i in range(1,sheet.max_row+1):
-    for j in range(1,sheet.max_column+1):
+    if sheet.cell(row=i,column=1).value=="fname3":
+     for j in range(1,sheet.max_column+1):
         print(sheet.cell(row=i,column=j).value)
