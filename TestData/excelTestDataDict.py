4b76c41631d0098b579a291368515a354519957c
srikanth <srikanthchelukala@gmail.com>
2023-07-13 11:07:30 +0530

    added new project PySelPytestProject

---
+++ TestData/excelTestDataDict.py
@@ -0,0 +1,21 @@
+import openpyxl
+book=openpyxl.load_workbook("C:\\Users\\srika\\OneDrive\\Desktop\\Atmntestdta\\exceldata.xlsx")
+dict={}
+sheet=book.active
+cell=sheet.cell(row=1,column=2)
+print(cell.value)
+# sheet.cell(row=2,column=2).value="csri"
+print(sheet.cell(row=2,column=2).value)
+print(sheet.max_row)
+print(sheet.max_column)
+print(sheet['A2'].value)
+# for i in range(1,sheet.max_row+1):
+#     print(sheet.cell(row=i,column=i).value)
+
+for i in range(1,sheet.max_row+1):
+    if sheet.cell(row=i,column=1).value=="fname3":
+     for j in range(1,sheet.max_column+1):
+        dict[sheet.cell(row=1,column=j).value]= sheet.cell(row=i,column=j).value
+
+print(dict)
+
